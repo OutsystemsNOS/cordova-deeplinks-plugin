b257f4337ec129f44ba5f0589aa3e7b900868517
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-05-15 14:14:31 +0100

    Update AppDelegate+CustomDeeplinksPlugin.m

--- src/ios/AppDelegate+CustomDeeplinksPlugin.m
+++ src/ios/AppDelegate+CustomDeeplinksPlugin.m
@@ -5,42 +5,81 @@
 
 - (void)notifyAppsFlyerWithUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
     Class appsFlyerClass = NSClassFromString(@"AppsFlyerLib");
-    if (appsFlyerClass) {
-        SEL sharedSelector = NSSelectorFromString(@"shared");
-        if ([appsFlyerClass respondsToSelector:sharedSelector]) {
-            id sharedLib = [appsFlyerClass performSelector:sharedSelector];
-            SEL continueSelector = NSSelectorFromString(@"continueUserActivity:restorationHandler:");
-            
-            if (sharedLib && [sharedLib respondsToSelector:continueSelector]) {
-                #pragma clang diagnostic push
-                #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-                [sharedLib performSelector:continueSelector withObject:userActivity withObject:restorationHandler];
-                #pragma clang diagnostic pop
-                NSLog(@"[CustomDeeplinks] Successfully forwarded Universal Link to AppsFlyer");
-            }
-        }
+    if (!appsFlyerClass) return;
+
+    SEL sharedSelector = NSSelectorFromString(@"shared");
+    if (![appsFlyerClass respondsToSelector:sharedSelector]) return;
+    
+    id sharedLib = [appsFlyerClass performSelector:sharedSelector];
+    if (!sharedLib) return;
+
+    SEL devKeySelector = NSSelectorFromString(@"appsFlyerDevKey");
+    SEL continueSelector = NSSelectorFromString(@"continueUserActivity:restorationHandler:");
+    if (![sharedLib respondsToSelector:continueSelector]) return;
+
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    NSString *devKey = [sharedLib respondsToSelector:devKeySelector] ? [sharedLib performSelector:devKeySelector] : nil;
+    #pragma clang diagnostic pop
+
+    if (devKey && devKey.length > 0) {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [sharedLib performSelector:continueSelector withObject:userActivity withObject:restorationHandler];
+        #pragma clang diagnostic pop
+        NSLog(@"[CustomDeeplinks] Warm Start: Universal Link forwarded to AppsFlyer immediately");
+    } else {
+        NSLog(@"[CustomDeeplinks] Cold Start: Waiting 2.5s for AppsFlyer JS initialization...");
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            #pragma clang diagnostic push
+            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [sharedLib performSelector:continueSelector withObject:userActivity withObject:restorationHandler];
+            #pragma clang diagnostic pop
+            NSLog(@"[CustomDeeplinks] Cold Start: Delayed Universal Link forwarded to AppsFlyer");
+        });
     }
 }
 
 - (void)notifyAppsFlyerWithURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
     Class appsFlyerClass = NSClassFromString(@"AppsFlyerLib");
-    if (appsFlyerClass) {
-        SEL sharedSelector = NSSelectorFromString(@"shared");
-        if ([appsFlyerClass respondsToSelector:sharedSelector]) {
-            id sharedLib = [appsFlyerClass performSelector:sharedSelector];
-            SEL openURLSelector = NSSelectorFromString(@"handleOpenURL:options:");
-            
-            if (sharedLib && [sharedLib respondsToSelector:openURLSelector]) {
-                #pragma clang diagnostic push
-                #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
-                [sharedLib performSelector:openURLSelector withObject:url withObject:options];
-                #pragma clang diagnostic pop
-                NSLog(@"[CustomDeeplinks] Successfully forwarded URL Scheme to AppsFlyer");
-            }
-        }
+    if (!appsFlyerClass) return;
+
+    SEL sharedSelector = NSSelectorFromString(@"shared");
+    if (![appsFlyerClass respondsToSelector:sharedSelector]) return;
+    
+    id sharedLib = [appsFlyerClass performSelector:sharedSelector];
+    if (!sharedLib) return;
+
+    SEL devKeySelector = NSSelectorFromString(@"appsFlyerDevKey");
+    SEL openURLSelector = NSSelectorFromString(@"handleOpenURL:options:");
+    if (![sharedLib respondsToSelector:openURLSelector]) return;
+
+    #pragma clang diagnostic push
+    #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+    NSString *devKey = [sharedLib respondsToSelector:devKeySelector] ? [sharedLib performSelector:devKeySelector] : nil;
+    #pragma clang diagnostic pop
+
+    if (devKey && devKey.length > 0) {
+        #pragma clang diagnostic push
+        #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+        [sharedLib performSelector:openURLSelector withObject:url withObject:options];
+        #pragma clang diagnostic pop
+        NSLog(@"[CustomDeeplinks] Warm Start: URL Scheme forwarded to AppsFlyer immediately");
+    } else {
+        NSLog(@"[CustomDeeplinks] Cold Start: Waiting 2.5s for AppsFlyer JS initialization...");
+        
+        dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(2.5 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+            #pragma clang diagnostic push
+            #pragma clang diagnostic ignored "-Warc-performSelector-leaks"
+            [sharedLib performSelector:openURLSelector withObject:url withObject:options];
+            #pragma clang diagnostic pop
+            NSLog(@"[CustomDeeplinks] Cold Start: Delayed URL Scheme forwarded to AppsFlyer");
+        });
     }
 }
 
+// Universal Link handler
 - (BOOL)application:(UIApplication *)application 
 continueUserActivity:(NSUserActivity *)userActivity 
 restorationHandler:(void (^)(NSArray *))restorationHandler {
@@ -52,7 +91,6 @@ restorationHandler:(void (^)(NSArray *))restorationHandler {
         return NO;
     }
 
-    // Change: Notify Appsflyer
     [self notifyAppsFlyerWithUserActivity:userActivity restorationHandler:restorationHandler];
 
     CustomDeeplinksPlugin *plugin = [self.viewController getCommandInstance:@"CustomDeeplinks"];
@@ -61,21 +99,18 @@ restorationHandler:(void (^)(NSArray *))restorationHandler {
     }
 
     NSLog(@"[CustomDeeplinks] URL: %@", userActivity.webpageURL.absoluteString);
-
     BOOL handled = [plugin handleUserActivity:userActivity];
-
     NSLog(@"[CustomDeeplinks] handleUserActivity result: %@", handled ? @"YES" : @"NO");
 
     return handled;
 }
 
+// Deep link (URL scheme) handler
 - (BOOL)application:(UIApplication *)app 
             openURL:(NSURL *)url 
             options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
 
     NSLog(@"[CustomDeeplinks] App opened via URL scheme: %@", url.absoluteString);
-
-    // ALTERAÇÃO: Notifica o AppsFlyer antes de terminar a execução
     [self notifyAppsFlyerWithURL:url options:options];
 
     return YES;
