bcb7f545ac33fcddb3ad20a8d2304d71302a1fb2
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-06-15 11:30:05 +0100

    Update AppDelegate+CustomDeeplinksPlugin.m

--- src/ios/AppDelegate+CustomDeeplinksPlugin.m
+++ src/ios/AppDelegate+CustomDeeplinksPlugin.m
@@ -3,60 +3,8 @@
 
 @implementation AppDelegate (CustomDeeplinksPlugin)
 
-- (BOOL)isAppsFlyerEnabled {
-    NSLog(@"[CustomDeeplinks] Starting isAppsFlyerEnabled check...");
-
-    NSString *configPath = [[NSBundle mainBundle] pathForResource:@"config" ofType:@"xml"];
-    if (!configPath) {
-        NSLog(@"[CustomDeeplinks] ERROR: config.xml path not found in mainBundle!");
-        return NO;
-    }
-    NSLog(@"[CustomDeeplinks] config.xml found at path: %@", configPath);
-
-    NSData *configData = [NSData dataWithContentsOfFile:configPath];
-    if (!configData) {
-        NSLog(@"[CustomDeeplinks] ERROR: Could not read data from config.xml!");
-        return NO;
-    }
-
-    NSString *configString = [[NSString alloc] initWithData:configData encoding:NSUTF8StringEncoding];
-    if (!configString) {
-        NSLog(@"[CustomDeeplinks] ERROR: Could not convert config.xml data to String!");
-        return NO;
-    }
-    
-    NSString *targetPattern = @"ENABLE_APPSFLYER_DEEEPLINKS";
-    NSRange range = [configString rangeOfString:targetPattern options:NSCaseInsensitiveSearch];
-    
-    if (range.location == NSNotFound) {
-        NSLog(@"[CustomDeeplinks] ERROR: Preference ENABLE_APPSFLYER_DEEEPLINKS completely missing from config.xml text!");
-        return NO;
-    }
-    
-    NSLog(@"[CustomDeeplinks] Preference key found in config.xml! Checking value...");
-
-    NSUInteger searchStart = range.location;
-    NSUInteger lengthToSearch = MIN(100, configString.length - searchStart);
-    NSString *subSegment = [configString substringWithRange:NSMakeRange(searchStart, lengthToSearch)];
-    
-    if ([subSegment rangeOfString:@"value=\"true\"" options:NSCaseInsensitiveSearch].location != NSNotFound) {
-        NSLog(@"[CustomDeeplinks] SUCCESS: isAppsFlyerEnabled is true");
-        return YES;
-    }
-
-    NSLog(@"[CustomDeeplinks] SUCCESS: isAppsFlyerEnabled is false (value is not true)");
-    return NO; 
-}
-
 - (void)notifyAppsFlyerWithUserActivity:(NSUserActivity *)userActivity restorationHandler:(void (^)(NSArray *))restorationHandler {
-    if (![self isAppsFlyerEnabled]) {
-        NSLog(@"[CustomDeeplinks] Forwarding blocked: ENABLE_APPSFLYER_DEEEPLINKS is false");
-        return;
-    }
-    
     Class appsFlyerClass = NSClassFromString(@"AppsFlyerLib");
-    CDVViewController* vc = (CDVViewController*)self.viewController; 
-
     if (!appsFlyerClass) return;
 
     SEL sharedSelector = NSSelectorFromString(@"shared");
@@ -94,14 +42,7 @@
 }
 
 - (void)notifyAppsFlyerWithURL:(NSURL *)url options:(NSDictionary<UIApplicationOpenURLOptionsKey,id> *)options {
-    if (![self isAppsFlyerEnabled]) {
-        NSLog(@"[CustomDeeplinks] Forwarding blocked: ENABLE_APPSFLYER_DEEEPLINKS is false");
-        return;
-    }
-    
     Class appsFlyerClass = NSClassFromString(@"AppsFlyerLib");
-    CDVViewController* vc = (CDVViewController*)self.viewController;
-    
     if (!appsFlyerClass) return;
 
     SEL sharedSelector = NSSelectorFromString(@"shared");
