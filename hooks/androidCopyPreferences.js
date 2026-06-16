1e1d646052b30257cfb25fb7085c370224123648
Miguel Rosa <79104255+MiguelRosaDev@users.noreply.github.com>
2026-06-16 11:20:31 +0100

    Update androidCopyPreferences.js

--- hooks/androidCopyPreferences.js
+++ hooks/androidCopyPreferences.js
@@ -1,42 +1,88 @@
-const et = require('elementtree');
-const path = require('path');
-const fs = require('fs');
-const { ConfigParser } = require('cordova-common');
-const { Console } = require('console');
+const et = require("elementtree");
+const path = require("path");
+const fs = require("fs");
+const { ConfigParser } = require("cordova-common");
+const { Console } = require("console");
 
 module.exports = function (context) {
-    var projectRoot = context.opts.cordova.project ? context.opts.cordova.project.root : context.opts.projectRoot;
-    var configXML = path.join(projectRoot, 'config.xml');
-    var configParser = new ConfigParser(configXML);
-    var app_domain_name = configParser.getGlobalPreference("APP_HOST");
+  var projectRoot = context.opts.cordova.project
+    ? context.opts.cordova.project.root
+    : context.opts.projectRoot;
+  var configXML = path.join(projectRoot, "config.xml");
+  var configParser = new ConfigParser(configXML);
 
-    //ANDROID
-    //go inside the AndroidManifest and change value for APP_DOMAIN_NAME
-    var manifestPath = path.join(projectRoot, 'platforms/android/app/src/main/AndroidManifest.xml');
-    var manifestFile = fs.readFileSync(manifestPath).toString();
-    var etreeManifest = et.parse(manifestFile);
+  var app_domain_name = configParser.getGlobalPreference("APP_HOST");
+  var app_domain_branded_name =
+    configParser.setGlobalPreference("APP_HOST_BRANDED");
 
-    var dataTags = etreeManifest.findall('./application/activity/intent-filter/data[@android:host="app_host"]');
-    for (var i = 0; i < dataTags.length; i++) {
-        var data = dataTags[i];
-        data.set("android:host", app_domain_name);
-    }
+  //ANDROID
+  //go inside the AndroidManifest and change value for APP_DOMAIN_NAME
+  var manifestPath = path.join(
+    projectRoot,
+    "platforms/android/app/src/main/AndroidManifest.xml",
+  );
+  var manifestFile = fs.readFileSync(manifestPath).toString();
+  var etreeManifest = et.parse(manifestFile);
 
-    var resultXmlManifest = etreeManifest.write();
-    fs.writeFileSync(manifestPath, resultXmlManifest);
-    
-    //change the config.xml
-    var configAndroidPath = path.join(projectRoot, 'platforms/android/app/src/main/res/xml/config.xml');
-    var configAndroidParser = new ConfigParser(configAndroidPath);
-    var oldDomainUriPrefix = configAndroidParser.getGlobalPreference("DOMAIN_URI_PREFIX");
-    var newDomainUriPrefix = oldDomainUriPrefix.replace('app_host', app_domain_name);
-    configAndroidParser.setGlobalPreference("DOMAIN_URI_PREFIX", newDomainUriPrefix);
-    configAndroidParser.write();
+  // Original App Host
+  var dataTags = etreeManifest.findall(
+    './application/activity/intent-filter/data[@android:host="app_host"]',
+  );
+  for (var i = 0; i < dataTags.length; i++) {
+    var data = dataTags[i];
+    data.set("android:host", app_domain_name);
+  }
 
-    //console.log('[CustomDeeplinks] AndroidManifest.xml atualizado com sucesso. Estado atual:');
-    //console.log(resultXmlManifest);
+  // Branded App Host
+  var dataTagsBranded = etreeManifest.findall(
+    './application/activity/intent-filter/data[@android:host="app_host_branded"]',
+  );
 
-    //console.log('[CustomDeeplinks] DOMAIN_URI_PREFIX updated in config.xml - Android:');
-    //console.log(`New value: ${newDomainUriPrefix}`);
+  for (var i = 0; i < dataTagsBranded.length; i++) {
+    var data_branded = dataTagsBranded[i];
+    data_branded.set("android:host", app_domain_branded_name);
+  }
 
+  var resultXmlManifest = etreeManifest.write();
+  fs.writeFileSync(manifestPath, resultXmlManifest);
+
+  //change the config.xml
+  var configAndroidPath = path.join(
+    projectRoot,
+    "platforms/android/app/src/main/res/xml/config.xml",
+  );
+  var configAndroidParser = new ConfigParser(configAndroidPath);
+
+  // Original App Host
+  var oldDomainUriPrefix =
+    configAndroidParser.getGlobalPreference("DOMAIN_URI_PREFIX");
+  var newDomainUriPrefix = oldDomainUriPrefix.replace(
+    "app_host",
+    app_domain_name,
+  );
+  configAndroidParser.setGlobalPreference(
+    "DOMAIN_URI_PREFIX",
+    newDomainUriPrefix,
+  );
+
+  // Branded App Host
+  var oldDomainUriPrefixBranded = configAndroidParser.getGlobalPreference(
+    "DOMAIN_URI_PREFIX_BRANDED",
+  );
+  var newDomainUriPrefixBranded = oldDomainUriPrefixBranded.replace(
+    "app_host_branded",
+    app_domain_branded_name,
+  );
+  configAndroidParser.setGlobalPreference(
+    "DOMAIN_URI_PREFIX_BRANDED",
+    newDomainUriPrefixBranded,
+  );
+
+  configAndroidParser.write();
+
+  //console.log('[CustomDeeplinks] AndroidManifest.xml atualizado com sucesso. Estado atual:');
+  //console.log(resultXmlManifest);
+
+  //console.log('[CustomDeeplinks] DOMAIN_URI_PREFIX updated in config.xml - Android:');
+  //console.log(`New value: ${newDomainUriPrefix}`);
 };
